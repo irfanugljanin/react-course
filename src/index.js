b6129c740d79849fb5b00542bb951dfe8066f758
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-08-08 14:22:49 +0200

    quizz

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/Quiz";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Registration from "./components/Registration Form/Registration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <Registration />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
