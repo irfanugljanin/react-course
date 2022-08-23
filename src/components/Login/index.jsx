484b982f6cfd36940e03501df9123d295e1300f8
Irfan Ugljanin <irfanikresoft@gmail.com>
2022-08-23 14:59:05 +0200

    Login Screen

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
 import "./style.css";
+import { useEffect, useState } from "react";
 
 const Login = () => {
+  let [auth, setAuth] = useState("signin");
+  let [signedin, setSignedin] = useState(false);
+
+  const changeAuth = () => {
+    setAuth(auth === "signin" ? "signup" : "signin");
+  };
+
+  if (auth === "signin") {
+    return (
+      <div className="container">
+        <div className="title">React</div>
+        <div className="login-signup">
+          <button>Login</button>
+          <button onClick={changeAuth}>SignUp</button>
+        </div>
+        <button className="signin-google">Sign in with Google</button>
+        <p>or</p>
+        <input type="email" placeholder="yours@example.com" />
+        <input type="password" placeholder="your password" />
+        <p>Don't remember your password?</p>
+        <button className="submit">sign in</button>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
-      <div className="title">React</div>
-      <div className="login-signup">
-        <button>Login</button>
-        <button>SignUp</button>
+      <div className="title"></div>
+      <div className="">
+        <label htmlFor="">Name</label>
+        <input type="text" />
+        <label htmlFor="">Email</label>
+        <input type="email" />
+        <label htmlFor="">Password</label>
+        <input type="password" />
+        <a onClick={changeAuth}>Already Registered? Click here!</a>
       </div>
-      <button className="signin-google">Sign in witg Google</button>
-      <p>or</p>
-      <input type="text" placeholder="yours@example.com" />
-      <input type="text" placeholder="your password" />
-      <p>Don't remember your password?</p>
-      <button className="submit">LOG IN ></button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,11 +5,7 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>
-);
+root.render(<Login />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
